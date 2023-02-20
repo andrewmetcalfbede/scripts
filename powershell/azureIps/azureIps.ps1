ce88bca1843f890ffb131576ed49a8384b84bbda
andrewmetcalfbede <andrew.metcalf@bedegaming.com>
2023-02-20 09:44:21 +0000

    edit

--- powershell/azureIps/azureIps.ps1
+++ powershell/azureIps/azureIps.ps1
@@ -1,26 +1,25 @@
 # https://learn.microsoft.com/en-us/powershell/module/az.network/get-aznetworkservicetag?view=azps-9.3.0
 # https://woivre.com/blog/2020/04/azure-service-ips
 
-function Get-AzureIps {
+function Get-AzureIpAddresses {
   param (
-    [Parameter(Mandatory = $true)]
-    [string]$location,
-  
-    [Parameter(Mandatory = $true)]
-    [string]$service
+      [Parameter(Mandatory = $true)]
+      [string]$location,
+      [Parameter(Mandatory = $true)]
+      [string]$service
   )
 
   try {
-    $serviceTags = Get-AzNetworkServiceTag -Location $location
-    $serviceName = $service+ "." +$location
-    $serviceTag  = $serviceTags.Values | Where-Object { $_.Name -eq $serviceName }
-    $ipAddresses = $serviceTag.Properties.AddressPrefixes | Where-Object { $_ -notmatch ':' }
-    return $ipAddresses
-    Write-Output $ipAddresses
+      $serviceName = $service+ "." +$location
+      $serviceTags = Get-AzNetworkServiceTag -Location $location
+      $serviceTag  = $serviceTags.Values | Where-Object { $_.Name -eq $serviceName }
+      $ipAddresses = $serviceTag.Properties.AddressPrefixes | Where-Object { $_ -notmatch ':' }
+      return $ipAddresses
   }
   catch {
-    Write-Output "Failed to get Azure IPs"
+      Write-Error "The Get-AzureIpAddresses function failed to get IPs for $service service in $location location: $($_.Exception.Message)"
   }
 }
 
-Write-Host $ipAddresses
+
+Get-AzureIpAddresses -location $location -service $service
